43d5e55628993224b8f12b76fe23e45002492ae0
chris2pher1503 <127755148+chris2pher1503@users.noreply.github.com>
2023-08-30 10:46:06 +0200

    Add files via upload

--- IMRT.py
+++ IMRT.py
@@ -13,15 +13,14 @@ import signal
 import time
 import sys
 import random
-from playsound import playsound
-playsound('barbie.mp3')
+
 
 LEFT = -1
 RIGHT = 1
 FORWARDS = 1
 BACKWARDS = -1
 DRIVING_SPEED = 100
-TURNING_SPEED = 100
+TURNING_SPEED = 150
 STOP_DISTANCE = 25
 
 def stop_robot(duration):
@@ -34,18 +33,20 @@ def stop_robot(duration):
 
 
 
-def drive_robot(direction, duration):
+def drive_robot(dist_1, dist_2, duration):
+        gain = 8
+        speed_motor_1 = dist_1 * gain
+        speed_motor_2 = dist_2 * gain
+        motor_serial.send_command(speed_motor_1, speed_motor_2)
+        time.sleep(duration)
+        
+def drive_turn():
     
-    speed = DRIVING_SPEED * direction
-    iterations = int(duration * 10)
-
-    for i in range(iterations):
-        motor_serial.send_command(speed, speed)
-        time.sleep(0.10)        
         
         
 
 def turn_robot(direction, duration):
+    iterations = int(duration * 10)
     
     for i in range(iterations):
         motor_serial.send_command(TURNING_SPEED * direction, -TURNING_SPEED * direction)
@@ -58,7 +59,7 @@ def turn_robot(direction, duration):
 
 
 # We want our program to send commands at 10 Hz (10 commands per second)
-execution_frequency = 100 #Hz
+execution_frequency = 200 #Hz
 execution_period = 1. / execution_frequency #seconds
 
 
@@ -103,29 +104,29 @@ while not motor_serial.shutdown_now :
 
 
     # Get and print readings from distance sensors
-    dist_1 = motor_serial.get_dist_1()
+    dist_1 = motor_serial.get_dist_4()
     dist_2 = motor_serial.get_dist_2()
     dist_3 = motor_serial.get_dist_3()
-    dist_4 = motor_serial.get_dist_4()
-    print("Dist hoyre:", dist_1, "   Dist venstre:", dist_2 "   Dist foran1:", dist_3 "   Dist foran2:", dist_4)
+    dist_4 = motor_serial.get_dist_1()
+    print("Dist hoyre:", dist_1, "   Dist venstre:", dist_2, "   Dist foran1:", dist_3, "   Dist foran2:", dist_4)
 
     # Check if there is an obstacle in the way
-    if dist_1>50 and dist_2>50 and dist_3>50: 
-        stop_robot(30)
+    if dist_1>100 and dist_2>100 and dist_3>100: 
+        stop_robot(60)
         #play song
         
     
     elif dist_1>50: 
-        drive_robot(FORWARDS,0.3)
-        turn_robot(RIGHT,1)
+        drive_robot(FORWARDS,0.1)
+        turn_robot(RIGHT,0.7)
     
     elif dist_1<10 and dist_3<10:
-        turn_robot(LEFT,1)
+        turn_robot(LEFT,0.7)
         
     elif dist_1<10 and dist_3<10 and dist_2<10: 
-        turn_robot(RIGHT,1)
-        turn_robot(RIGHT,1)
-    elif dist_3>10 or dist_4>10:
+        turn_robot(RIGHT,0.7)
+        turn_robot(RIGHT,0.7)
+    elif dist_3<10 or dist_4<10:
         turn_robot(RIGHT,1)
         
     
@@ -133,11 +134,8 @@ while not motor_serial.shutdown_now :
 
     else:
         # If there is nothing in front of the robot it continus driving forwards
-        drive_robot(FORWARDS, 0.1)
-        gain = 8
-        speed_motor_1 = dist_1 * gain
-        speed_motor_2 = dist_2 * gain
-        motor_serial.send_command(speed_motor_1, speed_motor_2)
+        drive_robot(dist_1,dist_2, 0.025)
+        
         
         
         
